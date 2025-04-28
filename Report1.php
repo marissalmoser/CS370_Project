f2aa216e3fe1e13f4ff43b7c8d31c042d1204658
Karkaranos <karkaranosyt@gmail.com>
2025-04-28 17:24:51 -0500

    end of meeting
    - added in-class notes to Report 1

--- Report1.php
+++ Report1.php
@@ -41,4 +41,137 @@ include 'header.php';
         </tbody>
     </table>
 
+<?php
+$connection_error = false;
+$connection_error_message = "";
+
+$con = @mysqli_connect("localhost", "pizza_user", "pizzapizza", "pizza_db");
+
+if(mysqli_connect_errno()) {
+    $connection_error = true;
+    $connection_error_message = "Failed to connect to MySql " . mysqli_connect_error();
+}
+
+function output_error($title, $error)
+{
+    echo "<span = \"color: red;\">\n";
+    echo "<h2>" . $title . "</h2>\n";
+    echo "<h4>" . $error . "</h4>\n";
+    echo "</span>\n";
+}
+?>
+
+    <html>
+    <head>
+        <title>Pizza Data Report</title>
+    </head>
+
+    </html>
+
+<?php(include_once("header.php")); ?>
+
+    <style>
+        .pizzaDataTable{
+            font-family : Calibri, monospace;
+            font-size : larger;
+            border-spacing : 0
+        }
+        .pizzaDataHeaderRow td{
+            font-weight : bold, padding-right : 20px
+
+        }
+        .pizzaDataRow td
+        {
+            border-bottom : 1px solid #888888; padding-left : 10px;
+        }
+        .pizzaDataDetailsCell
+        {
+            padding-left: 20px; font-size : medium;
+        }
+        .pizzaDataTable tr:nth-child(2n+2)
+        {
+            background-color : #cccccc;
+        }
+    </style>
+    if($connection_error) {
+    output_error("Database Connection Error! " . $connection_error_message);
+    }
+    else
+    {
+    function output_table_open()
+    {
+    echo "<table\n";
+    echo "<tr>\n";
+    echo "<td>Name</td>\n";
+    echo "<td>Age</td>\n";
+    echo "<td>Gender</td>\n";
+    echo "</tr>";
+    }
+
+    function output_table_close()
+    {
+    echo "</table>\n";
+    }
+
+    function output_person_row($name, $age, $gender)
+    {
+    echo "<tr class>\n";
+    echo "<td> . $name . </td>";
+    echo "<td>" . $age . "</td>";
+    echo "<td>" . $gender . "</td>";
+    echo "</tr>\n";
+    }
+
+
+
+    $query = " SELECT t0.mame, t0.age, t0.gender, t1.pizza, t2.pizzeria"
+    . " FROM Person t0"
+    . " LEFT OUTER JOIN Eats t1 on t0.name = t1.name"
+    . " LEFT OUTER JOIN Frequents t2 on t1.name = t2.name"
+    . " ORDER BY t0.name, t1.pizza, t2.pizzeria";
+    $result = mysqli_query($con, $query);
+    // used for selects, inserts, updates, and deletes
+
+    if(!$result) {
+    if(mysqli_errno($con)) {
+    output_error("Database retrieval Failed! " . mysqli_error($con));
+    }
+    else
+    {
+    echo "No pizza data found!\n";
+    }
+    }
+    else
+    {
+    output_table_open();
+
+    $last_name = null;
+    $pizzas = array();
+    $pizzerias = array();
+
+    while($row = $result ->fetch_array())
+    {
+    if($last_name != $row[" name"]) {
+    if ($last_name != null)
+    {
+    output_person_details_row($pizzas, $pizzerias);
+    }
+    output_person_row($row[" name"], $row[" age"], $row[" gender"]);
+
+    $pizzas[] = $row[" pizza"];
+    $pizzas[] = $row[" pizzeria"];
+
+    $last_name = $row[" name"];
+
+    }
+
+
+    }
+    output_person_details_row($pizzas, $pizzerias);
+
+    output_table_close();
+    }
+    }
+
+?>
 <?php include 'footer.php'; ?>
