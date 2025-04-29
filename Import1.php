ded9014c2844638045cf60331baaf462734fea36
Marissa Moser <marissalmoser@gmail.com>
2025-04-29 09:00:40 -0500

    import successful

--- Import1.php
+++ Import1.php
@@ -2,12 +2,12 @@
 include 'header.php';
 ?>
 
-    <!DOCTYPE html>
+<!DOCTYPE html>
 
-    <div class="container">
-        <h1>Welcome to My Website</h1>
-        <p>This is the import 1 page.</p>
-    </div>
+<div class="container">
+    <h1>Welcome to My Website</h1>
+    <p>This is the import 1 page.</p>
+</div>
 
 <?php
 
@@ -16,53 +16,53 @@ $import_attempted = false;
 $import_successful = false;
 $import_error_message = "";
 
-    if($_SERVER["REQUEST_METHOD"] == 'POST')
+//connect to server
+if($_SERVER["REQUEST_METHOD"] == 'POST') {
+    //echo "Posted!!!\n";
+    $import_attempted = true;
+
+    $mysqli = new mysqli("localhost", "root", "root3", "news");
+
+    if ($mysqli->connect_errno)
     {
-        //echo "Posted!!!\n";
-        $import_attempted = true;
-
-        $mysqli = new mysqli("localanchor","root","root3","news");
-
-        if ($mysqli -> connect_errno) {
-            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-            exit();
-        }
+        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+        exit();
     }
     else
     {
-        try
-        {
+        try {
             $contents = file_get_contents($_FILES["importFile"]["tmp_name"]);
             $lines = explode("\n", $contents);
 
-            foreach($lines as $line)
-            {
-                $parsed_csv_line = str_getcsv($line);
-                // do something with parsed data
+            foreach ($lines as $line) {
+                $parsed_csv_line = str_getcsv($line, ",", '"', "\\");
+                // TODO: do something with parsed data
             }
 
-        }
-        catch(Error $e)
+            $import_successful = true;
+
+        } catch (Error $e)
         {
-            $import_error_message = $e->getMessage() . " at: " . $e->getFile() . " line " . $e->getLine() ."<br/>";
+            $import_error_message = $e->getMessage() . " at: " . $e->getFile() . " line " . $e->getLine() . "<br/>";
         }
     }
-
+}
 ?>
 
 <?php
 
+//success and fail messages
 if($import_attempted){
     if($import_successful){
         ?>
-        <h1><span style = "color: green"> Import Succeeded! </span></h1>
+        <h1><span class="text-success"> Import Succeeded! </span></h1>
         #coun
         <?php
     }
     else
     {
         ?>
-        <h1><span style = "color: red"> Import Failed</span></h1>
+        <h1><span class="text-danger"> Import Failed</span></h1>
         <?php
         echo $import_error_message; ?>
         <br><br>
@@ -70,13 +70,14 @@ if($import_attempted){
     }
 }
 ?>
-    <form method = "post" enctype = "multipart/form-data">
-        <div class = "input-group mb-3">
-            File: <input type = "file" name = "importFile" />
-            <br><br>
-            <input type ="submit" value ="Upload Data" />
-        </div>
-    </form>
 
+//form
+<form method = "post" enctype = "multipart/form-data">
+    <div class = "input-group mb-3">
+        File: <input type = "file" name = "importFile" />
+        <br><br>
+        <input type ="submit" value ="Upload Data" />
     </div>
+</form>
+
 <?php include_once("footer.php"); ?>
